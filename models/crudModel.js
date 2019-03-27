093e8e6424db270a40e2129dcba987a6314f55e1
madhu <madhu.chaitanya@riktamtech.com>
2019-03-27 09:45:06 +0530

    few more queries done

--- models/crudModel.js
+++ models/crudModel.js
@@ -247,5 +247,345 @@ module.exports = {
                 resolve(result);
             });
         });
+    },
+    getListPropertyLatest: function () {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from latest_property order by latest_property_id ASC limit 6", function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPropertyDetailsById: function (id) {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from latest_property where list_property_id=" + id, function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPropertyPhotosLimit: function (id) {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from property_photos where property_id=" + id + " limit 1", function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPropertyPhotos: function (id) {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from property_photos where property_id=" + id, function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPropertyPhotoById: function (id) {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from property_photos where photo_id=" + id, function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPostRequirement: function () {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from post_requirement order by datetime DESC", function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result);
+            });
+        });
+    },
+    getPostRequirementCount: function () {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from post_requirement", function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result.length);
+            });
+        });
+    },
+    getPostRequirementById(postRequirementId) {
+        return new Promise(function (resolve, reject) {
+            con.query("select * from post_requirement where post_requirement_id=" + postRequirementId, function (error, result) {
+                if (error)
+                    console.log(error);
+                resolve(result.length);
+            });
+        });
+    },
+    getProperties: function (propertyType, propertySubType, city, locality, bedrooms, postedBy, minPrice, maxPrice, possession, ) {
+        var query = "SELECT * FROM list_property where list_property_id !=0";
+
+
+        query += " AND status='1' ORDER BY datetime DESC ";
+        console.log(query);
+        // if()
     }
+    /*
+
+	
+
+    function get_properties(){    	
+
+    	
+
+    	$sql = "SELECT * FROM list_property where list_property_id !=0";
+
+    	 
+
+    	if ($this->input->get('property_type')) {
+
+    		if ($this->input->get('property_type') != '0') {
+
+    			$sql .= " AND property_type='".$this->input->get('property_type')."'";
+
+    		}    		
+
+    	}
+
+    	if ($this->input->get('property_sub_type')) {
+
+    		$sql .= " AND property_sub_type='".$this->input->get('property_sub_type')."'";
+
+    	}
+
+    	if ($this->input->get('city')) {
+
+    		$sql .= " AND city='".$this->input->get('city')."'";
+
+    	}
+
+    	if ($this->input->get('locality')) {
+
+    		$sql .= " AND locality='".$this->input->get('locality')."'";
+
+    	}
+
+    	if ($this->input->get('bedrooms')) {
+
+    		$sql .= " AND bedrooms='".$this->input->get('bedrooms')."'";
+
+    	}
+
+    	if ($this->input->get('posted_by')) {
+
+    		$sql .= " AND posted_by='".$this->input->get('posted_by')."'";
+
+    	}
+
+    	
+
+    	if ($this->input->get('min_price')) {    			
+
+    		$sql .= " AND quoted_price >='".$this->input->get('min_price')."'";    		    		 
+
+    	}    	
+
+    	if ($this->input->get('max_price')) {
+
+    			$sql .= " AND quoted_price <='".$this->input->get('max_price')."'";
+
+    	}
+
+    	
+
+    	if ($this->input->get('possession')) {
+
+    		$sql .= " AND possession ='".$this->input->get('possession')."'";
+
+    	}
+
+    	    	 
+
+    	$sql .= " AND status='1' ORDER BY datetime DESC ";
+
+    	 	    	
+
+    	$query = $this->db->query($sql);
+
+    	return $query->result_array();
+
+    }
+
+    
+
+    function get_properties_count(){
+
+    	 
+
+    	$sql = "SELECT list_property_id FROM list_property where list_property_id!=0";
+
+    
+
+    	if ($this->input->get('property_type')) {
+
+    		if ($this->input->get('property_type') != '0') {
+
+    			$sql .= " AND property_type='".$this->input->get('property_type')."'";
+
+    		}
+
+    	}
+
+    	if ($this->input->get('property_sub_type')) {
+
+    		$sql .= " AND property_sub_type='".$this->input->get('property_sub_type')."'";
+
+    	}
+
+    	if ($this->input->get('city')) {
+
+    		$sql .= " AND city='".$this->input->get('city')."'";
+
+    	}
+
+    	if ($this->input->get('bedrooms')) {
+
+    		$sql .= " AND bedrooms='".$this->input->get('bedrooms')."'";
+
+    	}
+
+    	if ($this->input->get('posted_by')) {
+
+    		$sql .= " AND posted_by='".$this->input->get('posted_by')."'";
+
+    	}
+
+    	if ($this->input->get('min_price')) {    			
+
+    		$sql .= " AND quoted_price >='".$this->input->get('min_price')."'";    		    		 
+
+    	}    	
+
+    	if ($this->input->get('max_price')) {
+
+    			$sql .= " AND quoted_price <='".$this->input->get('max_price')."'";
+
+    	}
+
+    	if ($this->input->get('possession')) {
+
+    		$sql .= " AND possession ='".$this->input->get('possession')."'";
+
+    	}
+
+    	if ($this->input->get('search_type')) {
+
+    		$sql .= " AND want_to ='".$this->input->get('search_type')."'";
+
+    	}
+
+    	
+
+    	$sql .= " AND status='1'";
+
+        	 
+
+    	$query = $this->db->query($sql);
+
+    	return $query->num_rows();
+
+    }
+
+    
+
+    function get_hot_properties($city){
+
+    	     	
+
+    	$sql = "SELECT * FROM list_property where list_property_id !=0 and hot_property='1' and city='".$city."' ORDER BY datetime DESC";
+
+    
+
+       	$query = $this->db->query($sql);
+
+    	return $query->result_array();
+
+    }
+
+    
+
+    function get_similar_properties($list_property_id,$property_type,$city){
+
+             
+
+    	$this->db->like('property_type',$property_type);
+
+    	$this->db->like('city',$city);
+
+    	$this->db->where('list_property_id !=',$list_property_id);
+
+    	$this->db->where('status','1');
+
+    	$this->db->order_by('datetime','desc');
+
+    	$this->db->limit('4');
+
+    	
+
+    	$query = $this->db->get('list_property');
+
+    	return $query->result_array();
+
+    }
+
+    
+
+    function get_similar_projects($project_id,$project_type,$city){
+
+    
+
+        $this->db->like('project_type',$project_type);
+
+    	$this->db->like('city',$city);
+
+    	$this->db->where('project_id !=',$project_id);
+
+    	$this->db->where('project_status','1');    	
+
+    	$this->db->order_by('datetime','desc');
+
+    	$this->db->limit('4');
+
+    	 
+
+    	$query = $this->db->get('projects');
+
+    	return $query->result_array();
+
+    }
+
+    
+
+    function get_similar_rent_properties($rent_id,$property_type,$city){
+
+    
+
+        $this->db->like('property_type',$property_type);
+
+    	$this->db->like('city',$city);
+
+    	$this->db->where('rent_id !=',$rent_id);
+
+    	$this->db->where('status','1');
+
+    	$this->db->order_by('datetime','desc');
+
+    	$this->db->limit('4');
+
+    	 
+
+    	$query = $this->db->get('rent');
+
+    	return $query->result_array();
+
+    }
+    */
 };
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var crudModel = require("../models/crudModel");
 
 router.get('/', function (req, res, next) {
-  // crudModel.getPlotDetails(5).then(function (response) {
-  //   console.log(response);
-  // });
+  crudModel.getProperties().then(function (response) {
+    console.log(response);
+  });
   // console.log("----------------------");
   // crudModel.getPropertyId("spaces", "spaces4all@gmail.com", 4, 7, 1, 7, "2015-11-29 01:03:23").then(function (response) {
   //   console.log(response);
